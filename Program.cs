c2fa16091ff0c65607c3d615c0cfc9652f351405
Patricio <patribernabeu064@gmail.com>
2025-02-28 22:05:11 +0100

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hada_p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //primeros cambios
+        }
+    }
+}
