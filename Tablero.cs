be2a41f7b0ebf415994e7e0ac440b132ab3ae129
Patricio <patribernabeu064@gmail.com>
2025-03-07 13:51:10 +0100

    29523336F añado la clase Tablero.cs y primeras modificaciones

---
+++ Tablero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Tablero
+    {
+        public int TamTablero { get; private set; }
+
+        private List<Coordenada> coordenadasDisparadas;
+        private List<Coordenada> coordenadasTocadas;
+        private List<Barco> barcos;
+        private List<Barco> barcosEliminados;
+        private Dictionary<Coordenada, string> casillasTablero;
+
+        public event EventHandler<EventArgs> eventoFinPartida;
+
+        public Tablero(int tamTablero, List<Barco> barcos)
+        {
+            if (tamTablero < 4 || tamTablero > 9)
+                throw new ArgumentException("El tamaño del tablero debe estar entre 4 y 9.");
+
+            TamTablero = tamTablero;
+            this.barcos = barcos;
+            coordenadasDisparadas = new List<Coordenada>();
+            coordenadasTocadas = new List<Coordenada>();
+            barcosEliminados = new List<Barco>();
+            casillasTablero = new Dictionary<Coordenada, string>();
+
+            foreach (var barco in this.barcos)
+            {
+                barco.eventoTocado += cuandoEventoTocado;
+                barco.eventoHundido += cuandoEventoHundido;
+            }
+
+            inicializaCasillasTablero();
+        }
+    }
+}
